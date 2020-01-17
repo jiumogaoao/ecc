2de9d595cefbf95ae29aafc6b1806f86a655bb91
jiumogaoao <394127821@qq.com>
2020-01-17 19:06:07 +0800

    1
    1

--- mixin/all.js
+++ mixin/all.js
@@ -1,53 +1,34 @@
-import headCP from '@/components/head_CP.vue'
+var browser = {
+            versions: function() {
+                var u = navigator.userAgent, app = navigator.appVersion;
+                return {     //移动终端浏览器版本信息
+                    trident: u.indexOf('Trident') > -1, //IE内核
+                    presto: u.indexOf('Presto') > -1, //opera内核
+                    webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+                    gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+                    mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+                    ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+                    android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器
+                    iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+                    iPad: u.indexOf('iPad') > -1, //是否iPad
+                    webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
+                };
+            } (),
+            language: (navigator.browserLanguage || navigator.language).toLowerCase()
+        }
 module.exports = {
-	components:{headCP},
 	data() {
 				return {
-					ww:0,
-					wh:0
+					phone:browser.versions.mobile
 				}
 				},
 	mounted(){
-		let that = this;
-		uni.getSystemInfo({
-		    success: function (res) {
-				that.ww = res.windowWidth
-				that.wh = res.windowHeight
-		    }
-		});
-		uni.onWindowResize((res)=>{
-			that.ww = res.size.windowWidth
-			that.wh = res.size.windowHeight
-		})
+
 	},
 	computed:{
-		w(){
-			if(this.ww > 1200){
-				return this.ww;
-			}else{
-				return 1200;
-			}
-		},
-		s(){
-			if(this.ww > 1200){
-				return 1;
-			}else{
-				return this.ww/1200
-			}
-		},
-		h(){
-			console.log()
-			return this.wh/this.s;
-		}
+
 	},
 	methods: {
-		back(){
-			uni.navigateBack();
-		},
-		go(url){
-			uni.navigateTo({
-			    url: url
-			});
-		}
+
 	},
 }
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
-		<view class="content">
-			<image src="/static/newWeb0.png" mode="widthFix"></image>
+		<view :class="{content:1,phone:phone}">
+			<image :src="phone?'/static/phoneIndex.png':'/static/newWeb0.png'" mode="widthFix"></image>
 			<input class="name"/>
 			<input class="password" password="true"/>
 			<view class="button" @click="c"></view>
@@ -9,7 +9,9 @@
 </template>
 
 <script>
+	import all from "@/mixin/all"
 	export default {
+		mixins: [all],
 		data() {
 			return {
 				
@@ -59,6 +61,35 @@
 			width: 74rpx;
 			height: 12rpx;
 			background-color: transparent;
+		}
+		&.phone{
+			input{
+				position: absolute;
+				width: 323rpx;
+				height: 53rpx;
+				border: 0;
+				outline: none;
+				background-color: transparent;
+				text-align: center;
+				color: #fff;
+				font-size: 18rpx;
+				&.name{
+					top:427rpx;
+					left: 222rpx;
+				}
+				&.password{
+					top:545rpx;
+					left: 222rpx;
+				}
+			}
+			.button{
+				position: absolute;
+				top:615rpx;
+				left: 222rpx;
+				width: 323rpx;
+				height: 53rpx;
+				background-color: transparent;
+			}
 		}
 	}
 </style>
